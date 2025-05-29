472488dee754a176cbba94aa10887f82c3305706
ls626262 <prouser4680989@gmail.com>
2025-05-29 07:37:25 -0400

    Add files via upload

---
+++ code0.js
@@ -0,0 +1,37 @@
+gdjs.Untitled_32sceneCode = {};
+gdjs.Untitled_32sceneCode.localVariables = [];
+gdjs.Untitled_32sceneCode.GDbackgroyundObjects1= [];
+gdjs.Untitled_32sceneCode.GDbackgroyundObjects2= [];
+
+
+gdjs.Untitled_32sceneCode.eventsList0 = function(runtimeScene) {
+
+{
+
+
+let isConditionTrue_0 = false;
+{
+{gdjs.evtTools.window.openURL("https://ls626262.github.io/tab-spammer", runtimeScene);
+}}
+
+}
+
+
+};
+
+gdjs.Untitled_32sceneCode.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+gdjs.Untitled_32sceneCode.GDbackgroyundObjects1.length = 0;
+gdjs.Untitled_32sceneCode.GDbackgroyundObjects2.length = 0;
+
+gdjs.Untitled_32sceneCode.eventsList0(runtimeScene);
+gdjs.Untitled_32sceneCode.GDbackgroyundObjects1.length = 0;
+gdjs.Untitled_32sceneCode.GDbackgroyundObjects2.length = 0;
+
+
+return;
+
+}
+
+gdjs['Untitled_32sceneCode'] = gdjs.Untitled_32sceneCode;
